0ccdd30b42378d7bdbe235c03b11b024906598e8
sam pandey <shamikantpandey559@gmail.com>
2023-03-08 05:00:13 +0530

    initilized my backend and front-end

---
+++ front-end/src/components/UserProfile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ankit from "../images/ankit.jpeg";
+function userProfile() {
+  return (
+    <>
+          <div className='container user-profile'>
+              <form method=''></form>
+          </div>
+    </>
+  )
+} 
+
+export default userProfile
